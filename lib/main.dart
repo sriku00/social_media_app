97a83f93055839202f6ba6a9d823bbf64b249c29
s <51485083+sriku00@users.noreply.github.com>
2021-01-06 15:55:56 +0530

    yes

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,27 @@
 import 'package:flutter/material.dart';
-
-// Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(App());
+Future<void> main() async {
+  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
-class App extends StatelessWidget {
-  // Create the initialization Future outside of `build`:
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-
-      future: _initialization,
-      builder: (context, snapshot) {
-
-        if (snapshot.hasError) {
-          return Text("Something Wrong");
-        }
-
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Text("");
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return CircularProgressIndicator();
-      },
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
     );
   }
 }
