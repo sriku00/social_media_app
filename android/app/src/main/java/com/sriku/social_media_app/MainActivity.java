7891c76f537026fadf4db88e028bbe135826b6ae
s <51485083+sriku00@users.noreply.github.com>
2021-01-07 12:15:22 +0530

    yes

--- android/app/src/main/java/com/sriku/social_media_app/MainActivity.java
+++ android/app/src/main/java/com/sriku/social_media_app/MainActivity.java
@@ -2,5 +2,8 @@ package com.sriku.social_media_app;
 
 import io.flutter.embedding.android.FlutterActivity;
 
+import android.os.Build;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 public class MainActivity extends FlutterActivity {
 }
--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -4,9 +4,14 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       body: Center(
         child: RichText(
-          text: TextSpan(text: "The", children: [TextSpan(text: "Socail")]),
+          text: TextSpan(text: "The", children: [
+            TextSpan(
+                text: " Socail",
+                style: TextStyle(color: Theme.of(context).accentColor))
+          ]),
         ),
       ),
     );
