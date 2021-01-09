a4532da356d75174cfc58b999718bdccb76a50a8
s <51485083+sriku00@users.noreply.github.com>
2021-01-09 16:22:57 +0530

    yes

--- lib/Screens/landing_screen.dart
+++ lib/Screens/landing_screen.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/all.dart';
+import 'package:social_media_app/Services/landing_screen_services.dart';
 import 'package:social_media_app/Utils/colors_utils.dart';
 
 // ignore: must_be_immutable
-class LandingScreen extends StatelessWidget {
+class LandingScreen extends HookWidget {
   ConstantColors constantColors = ConstantColors();
   @override
   Widget build(BuildContext context) {
+    final provider = useProvider(landingScreenServices);
     return Scaffold(
       backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          bodyColor(),
+         provider.bodyImage(context),
+         provider.tagLineWidget(context),
+         provider.buttonLanding(context),
+         provider.landingFootBar(context)
+        ],
+      ),
     );
   }
 }
 
-bodyColor() => Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0.3, 0.9],
-            colors: []),
+Widget bodyColor() {
+  ConstantColors constantColors = ConstantColors();
+  return Container(
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        stops: [0.5, 0.9],
+        colors: [
+          constantColors.darkColor,
+          constantColors.blueGreyColor,
+        ],
       ),
-    );
+    ),
+  );
+}
---
+++ lib/Services/landing_screen_services.dart
@@ -0,0 +1,159 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:eva_icons_flutter/icon_data.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:social_media_app/Utils/colors_utils.dart';
+
+final landingScreenServices =
+    ChangeNotifierProvider<LandingScreenServices>((ref) {
+  return LandingScreenServices();
+});
+
+class LandingScreenServices extends ChangeNotifier {
+  ConstantColors constantColors = ConstantColors();
+  // body Image widget
+  Widget bodyImage(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.65,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/login.png"),
+        ),
+      ),
+    );
+  }
+
+  // Tag Line Widget
+  Widget tagLineWidget(BuildContext context) {
+    return Positioned(
+        top: 500,
+        left: 20,
+        child: Container(
+          constraints: BoxConstraints(maxHeight: 170),
+          child: RichText(
+            text: TextSpan(children: [
+              TextSpan(
+                text: " ARe",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: " YoU",
+                style: TextStyle(
+                    color: Colors.lightBlue,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: " SoCiaL",
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: " ?",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold),
+              )
+            ]),
+          ),
+        ));
+  }
+
+  // Bottom Widget of landing page
+
+  Widget buttonLanding(BuildContext context) {
+    return Positioned(
+      top: 630,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GestureDetector(
+              child: Container(
+                height: 40,
+                width: 80,
+                child: Center(
+                    child: Icon(
+                  EvaIcons.emailOutline,
+                  color: constantColors.yellowColor,
+                  size: 35,
+                )),
+                decoration: BoxDecoration(
+                  border:
+                      Border.all(color: constantColors.yellowColor, width: 2),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+            ),
+            GestureDetector(
+              child: Container(
+                height: 40,
+                width: 80,
+                child: Center(
+                    child: Icon(
+                  EvaIcons.googleOutline,
+                  color: constantColors.redColor,
+                  size: 35,
+                )),
+                decoration: BoxDecoration(
+                  border: Border.all(color: constantColors.redColor, width: 2),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+            ),
+            GestureDetector(
+              child: Container(
+                height: 40,
+                width: 80,
+                child: Center(
+                    child: Icon(
+                  EvaIcons.facebookOutline,
+                  color: constantColors.blueColor,
+                  size: 35,
+                )),
+                decoration: BoxDecoration(
+                  border: Border.all(color: constantColors.blueColor, width: 2),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget landingFootBar(BuildContext context) {
+    return Positioned(
+      top: 750,
+      left: 40,
+      right: 20,
+      child: Container(
+        child: Column(
+          children: [
+            Text(
+              "By continuing you  agreed to the Social app Terms , ",
+              style: TextStyle(
+                color: constantColors.greyColor,
+              ),
+            ),
+            Text(
+              " Services & Privacy Policy ",
+              style: TextStyle(
+                color: constantColors.greyColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
